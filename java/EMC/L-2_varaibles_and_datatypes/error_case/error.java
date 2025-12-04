f923076b309ff08074335cd57d79cbfa9fb1b8c2
Akilesh Balamurugan <akileshworkmailz@gmail.com>
2025-12-04 22:04:32 +0530

    java

---
+++ java/EMC/L-2_varaibles_and_datatypes/error_case/error.java
@@ -0,0 +1,14 @@
+
+public class error {
+    public static void main(String[] args) {
+        int stNumber = 10; // Invalid variable name (cannot start with a digit)
+        System.out.println(stNumber);
+    }
+public class error1 {
+    public static int a = 5;
+    public static void main(String[] args) {
+        System.out.println(a);
+    }
+
+}
+}
---
+++ java/lession1/java1.java
@@ -0,0 +1,8 @@
+package lession1;
+
+public class java1 {
+    public static void main(String[]args){
+        String name = "akilesh";
+        System.out.println("hello "+name);
+    }
+}
